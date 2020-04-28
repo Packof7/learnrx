34cfc780c9ad1d870d329b0caa3a7d74f51da00f
Christoph Häckel <ch@koeln.de>
2020-04-28 14:40:33 +0200

    Format WebParts

--- packof7/night-of-the-pack-2020/src/webparts/intro/IntroWebPart.ts
+++ packof7/night-of-the-pack-2020/src/webparts/intro/IntroWebPart.ts
@@ -1,31 +1,38 @@
-import { Version } from '@microsoft/sp-core-library';
+import { Version } from "@microsoft/sp-core-library";
 import {
   BaseClientSideWebPart,
   IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-webpart-base';
-import { escape } from '@microsoft/sp-lodash-subset';
+  PropertyPaneTextField,
+} from "@microsoft/sp-webpart-base";
+import { escape } from "@microsoft/sp-lodash-subset";
 
-import styles from './IntroWebPart.module.scss';
-import * as strings from 'IntroWebPartStrings';
+import styles from "./IntroWebPart.module.scss";
+import * as strings from "IntroWebPartStrings";
 
 export interface IIntroWebPartProps {
   description: string;
 }
 
-export default class IntroWebPart extends BaseClientSideWebPart<IIntroWebPartProps> {
-
+export default class IntroWebPart extends BaseClientSideWebPart<
+  IIntroWebPartProps
+> {
   public render(): void {
     this.domElement.innerHTML = `
-      <div class="${ styles.intro }">
-        <div class="${ styles.container }">
-          <div class="${ styles.row }">
-            <div class="${ styles.column }">
-              <span class="${ styles.title }">Welcome to SharePoint!</span>
-              <p class="${ styles.subTitle }">Customize SharePoint experiences using Web Parts.</p>
-              <p class="${ styles.description }">${escape(this.properties.description)}</p>
-              <a href="https://aka.ms/spfx" class="${ styles.button }">
-                <span class="${ styles.label }">Learn more</span>
+      <div class="${styles.intro}">
+        <div class="${styles.container}">
+          <div class="${styles.row}">
+            <div class="${styles.column}">
+              <span class="${
+                styles.title
+              }">Welcome to SharePoint! Plain JS/TS</span>
+              <p class="${
+                styles.subTitle
+              }">Customize SharePoint experiences using Web Parts.</p>
+              <p class="${styles.description}">${escape(
+      this.properties.description
+    )}</p>
+              <a href="https://aka.ms/spfx" class="${styles.button}">
+                <span class="${styles.label}">Learn more</span>
               </a>
             </div>
           </div>
@@ -34,7 +41,7 @@ export default class IntroWebPart extends BaseClientSideWebPart<IIntroWebPartPro
   }
 
   protected get dataVersion(): Version {
-    return Version.parse('1.0');
+    return Version.parse("1.0");
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
@@ -42,20 +49,20 @@ export default class IntroWebPart extends BaseClientSideWebPart<IIntroWebPartPro
       pages: [
         {
           header: {
-            description: strings.PropertyPaneDescription
+            description: strings.PropertyPaneDescription,
           },
           groups: [
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
-            }
-          ]
-        }
-      ]
+                PropertyPaneTextField("description", {
+                  label: strings.DescriptionFieldLabel,
+                }),
+              ],
+            },
+          ],
+        },
+      ],
     };
   }
 }
--- packof7/night-of-the-pack-2020/src/webparts/packClient/components/PackClient.tsx
+++ packof7/night-of-the-pack-2020/src/webparts/packClient/components/PackClient.tsx
@@ -1,25 +1,32 @@
-import * as React from 'react';
-import styles from './PackClient.module.scss';
-import { IPackClientProps } from './IPackClientProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import * as React from "react";
+import styles from "./PackClient.module.scss";
+import { IPackClientProps } from "./IPackClientProps";
+import { escape } from "@microsoft/sp-lodash-subset";
 
-export default class PackClient extends React.Component < IPackClientProps, {} > {
+export default class PackClient extends React.Component<IPackClientProps, {}> {
   public render(): React.ReactElement<IPackClientProps> {
-    return(
-      <div className = { styles.packClient } >
-  <div className={styles.container}>
-    <div className={styles.row}>
-      <div className={styles.column}>
-        <span className={styles.title}>Welcome to SharePoint!</span>
-        <p className={styles.subTitle}>Customize SharePoint experiences using Web Parts.</p>
-        <p className={styles.description}>{escape(this.props.description)}</p>
-        <a href='https://aka.ms/spfx' className={styles.button}>
-          <span className={styles.label}>Learn more</span>
-        </a>
+    return (
+      <div className={styles.packClient}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.column}>
+              <span className={styles.title}>
+                Welcome to SharePoint! This is React.
+              </span>
+              <p className={styles.subTitle}>
+                Customize SharePoint experiences using Web Parts.
+              </p>
+              <p className={styles.description}>
+                {escape(this.props.description)}
+              </p>
+              <a href="https://aka.ms/spfx" className={styles.button}>
+                <span className={styles.label}>Learn more</span>
+              </a>
+              <Separator></Separator>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-      </div >
     );
   }
 }
