193881d455ae80a3848b89f589f4703ddc1c320f
Christoph Häckel <ch@koeln.de>
2020-04-28 12:31:34 +0200

    Generate Intro WebPart with yeoman

---
+++ packof7/night-of-the-pack-2020/src/webparts/intro/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IIntroWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'IntroWebPartStrings' {
+  const strings: IIntroWebPartStrings;
+  export = strings;
+}
