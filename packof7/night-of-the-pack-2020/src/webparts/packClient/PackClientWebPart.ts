594bd9c2075d3e023fbed10152b1e1a68dbe39a8
Christoph Häckel <ch@koeln.de>
2020-04-28 14:28:30 +0200

    Generate PackClient WebPart

---
+++ packof7/night-of-the-pack-2020/src/webparts/packClient/PackClientWebPart.ts
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import { Version } from '@microsoft/sp-core-library';
+import {
+  BaseClientSideWebPart,
+  IPropertyPaneConfiguration,
+  PropertyPaneTextField
+} from '@microsoft/sp-webpart-base';
+
+import * as strings from 'PackClientWebPartStrings';
+import PackClient from './components/PackClient';
+import { IPackClientProps } from './components/IPackClientProps';
+
+export interface IPackClientWebPartProps {
+  description: string;
+}
+
+export default class PackClientWebPart extends BaseClientSideWebPart<IPackClientWebPartProps> {
+
+  public render(): void {
+    const element: React.ReactElement<IPackClientProps > = React.createElement(
+      PackClient,
+      {
+        description: this.properties.description
+      }
+    );
+
+    ReactDom.render(element, this.domElement);
+  }
+
+  protected onDispose(): void {
+    ReactDom.unmountComponentAtNode(this.domElement);
+  }
+
+  protected get dataVersion(): Version {
+    return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: strings.PropertyPaneDescription
+          },
+          groups: [
+            {
+              groupName: strings.BasicGroupName,
+              groupFields: [
+                PropertyPaneTextField('description', {
+                  label: strings.DescriptionFieldLabel
+                })
+              ]
+            }
+          ]
+        }
+      ]
+    };
+  }
+}
---
+++ packof7/night-of-the-pack-2020/src/webparts/packClient/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPackClientWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'PackClientWebPartStrings' {
+  const strings: IPackClientWebPartStrings;
+  export = strings;
+}
